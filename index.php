0b24ff560f5e89cb752590084a76a1bb55e6879d
Gabriel <gabrielsprata@hotmail.com>
2023-10-08 00:19:10 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+  <title>Juice Shop</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700;900&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body class="grey darken-3">
+  <?php
+  include 'navbar.php';
+  ?>
+  <div class="container">
+    <div class="row">
+      <h3 id="titleHome" class="white-text">Conheça nossos produtos!</h3>
+    </div>
+    <div class="row">
+
+    </div>
+  </div>
+
+  <?php
+    include 'footer.php';
+  ?>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,41 @@
+<nav class="blue-grey darken-2" role="navigation">
+    <div class="nav-wrapper container row">
+        <a id="logo-container" href="#" class="col s3 valign-wrapper">
+            <img src="img/JuiceShop_Logo.png" alt="OWASP Juice Shop Logo" id="logoNavbar">
+            <p>OWASP Juice Shop</p>
+        </a>
+        <div class="col s6">
+
+            <ul class="right hide-on-med-and-down">
+                <li>
+                    <a class="dropdown-trigger" href="#!" data-target="dropdown1">
+                        Usuário <i class="material-icons right">arrow_drop_down</i>
+                    </a>
+                </li>
+            </ul>
+            <ul id="dropdown1" class="dropdown-content">
+                <li><a href="#!">one</a></li>
+                <li><a href="#!">two</a></li>
+                <li class="divider"></li>
+                <li><a href="#!">three</a></li>
+            </ul>
+
+            <!-- <ul class="right hide-on-med-and-down">
+                <li><a href="#">Navbar Link</a></li>
+            </ul> -->
+
+
+
+        </div>
+
+        <div class="col s3">
+            <form id="productSearch">
+                <div class="input-field">
+                    <input id="search" type="search" required placeholder="Pesquisar...">
+                    <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                    <i class="material-icons">close</i>
+                </div>
+            </form>
+        </div>
+    </div>
+</nav>
